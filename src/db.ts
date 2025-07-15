57a7dc683b15b99aac0489dae50e7eb2a92d1c30
deytapas <93096230+deytapas@users.noreply.github.com>
2025-07-16 03:56:16 +0530

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,4 @@
 import mongoose, { Schema, model } from "mongoose";
-mongoose.connect("mongodb+srv://deytapas374:syLPAYRsmQAJuobh@cluster0.j1yom9v.mongodb.net/SecondBrain");
 
 const UserSchema = new Schema({
     username: {type: String, unique: true},
